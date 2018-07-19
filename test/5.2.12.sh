e806e911ab6bf3938ab36ee0650258578c0c8683
Phil Massyn <massyn@users.noreply.github.com>
2018-07-19 21:41:28 +0900

    Create 5.2.12.sh

---
+++ test/5.2.12.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# 5.2.12 Ensure SSH Idle Timeout Interval is configured (Scored)
+
+CAI=$(grep "^ClientAliveInterval" /etc/ssh/sshd_config | awk {'print $2'})
+
+if [[ $CAI -eq '' || $CAI -lt 1 || $CAI -gt 300 ]]; then
+        echo ClientAliveInterval = $CAI
+        exit 1
+fi
+
+CACM=$(grep "^ClientAliveCountMax" /etc/ssh/sshd_config |awk {'print $2'})
+
+if [[ $CACM -eq '' || $CACM -gt 3 ]]; then
+        echo ClientAliveCountMax = $CACM
+        exit 1
+fi
