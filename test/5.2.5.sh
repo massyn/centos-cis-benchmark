fd23fd371c70c1d0f27eaacd4ef71f8683d751ae
Phil Massyn <massyn@users.noreply.github.com>
2018-07-19 21:53:56 +0900

    Create 5.2.5.sh

---
+++ test/5.2.5.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# 5.2.5 Ensure SSH MaxAuthTries is set to 4 or less (Scored)
+
+MAT=$(grep "^MaxAuthTries" /etc/ssh/sshd_config |awk {'print $1'})
+
+if [[ $MAT -eq '' || $MAT -gt 4 ]]; then
+        echo MaxAuthTries = $MAT
+        exit 1
+fi
