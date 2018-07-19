d22b8d3f23df2d19e38191b1d4f9e3b64f0c787d
Phil Massyn <massyn@users.noreply.github.com>
2018-07-19 22:58:00 +0900

    Create 5.4.2.sh

---
+++ test/5.4.2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 5.4.2 Ensure system accounts are non-login (Scored)
+
+CNT=$(egrep -v "^\+" /etc/passwd | awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $3<1000 && $7!="/sbin/nologin" && $7!="/bin/false") {print}' |wc -l)
+
+if [[ $CNT -ne 0 ]]; then
+        exit 1
+fi
