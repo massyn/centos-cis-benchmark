06f9b7a489d202897c40f722e597c69c475e828e
Phil Massyn <massyn@users.noreply.github.com>
2018-07-19 22:11:00 +0900

    Update 6.1.2.sh

--- test/6.1.2.sh
+++ test/6.1.2.sh
@@ -2,4 +2,4 @@
 
 # 6.1.2 Ensure permissions on /etc/passwd are configured (Scored)
 
-stat -L -c "%a %u %g" /etc/passwd | grep -q '644 0 0$' || exit $?
+stat -L -c "%a %u %g" /etc/passwd | grep -q "644 0 0$" || exit $?
