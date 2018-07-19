3f5c74ef96499cb81d47a027b13398382991a13c
Phil Massyn <massyn@users.noreply.github.com>
2018-07-19 22:13:20 +0900

    Update 6.1.8.sh

--- test/6.1.8.sh
+++ test/6.1.8.sh
@@ -2,4 +2,4 @@
 
 # 6.1.8 Ensure permissions on /etc/group- are configured (Scored)
 
-stat -L -c "%a %u %g" /etc/group- | grep -q '644 0 0$' || exit $?
+stat -L -c "%a %u %g" /etc/group- | grep -q "644 0 0$" || exit $?
