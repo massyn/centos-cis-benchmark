15ef07ca3bf22a35d56343dc6ffd335d722e6edf
Phil Massyn <massyn@users.noreply.github.com>
2018-07-19 22:49:36 +0900

    Create 5.4.1.2.sh

---
+++ test/5.4.1.2.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# 5.4.1.2 Ensure minimum days between password changes is 7 or more (Scored)
+
+PMD=$(grep -E "^PASS_MIN_DAYS" /etc/login.defs | awk {'print $2'})
+
+if [[ $PMD -eq '' || $PMD -lt 7 ]]; then
+        echo PASS_MIN_DAYS = $PMD
+        exit 1
+fi
+
+for i in $(egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1 ); do
+        UPA=$(chage --list $i | grep "Minimum number of days between password change" | cut -d: -f2)
+        if [[ $UPA -lt 7 ]]; then
+                echo $i days between password change = $UPA
+                exit 1
+        fi
+done
