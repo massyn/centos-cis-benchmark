c99c4b5d44ea7c020bfc4dc7c2ff11d30fbab1fc
Phil Massyn <massyn@users.noreply.github.com>
2018-07-19 21:33:34 +0900

    Create 5.2.13.sh

---
+++ test/5.2.13.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 5.2.13 Ensure SSH LoginGraceTime is set to one minute or less (Scored)
+
+LGT=$(grep "^LoginGraceTime" /etc/ssh/sshd_config | awk {'print $2'})
+if [[ $LGT -eq '' || $LGT -gt 60 || $LGT -lt 1 ]]; then
+        echo LoginGraceTime is $LGT
+        exit 1
+fi
