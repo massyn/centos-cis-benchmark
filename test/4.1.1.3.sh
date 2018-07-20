63197086affaf232719ed37623724845e1c1d8d1
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 16:48:32 +0900

    Create 4.1.1.3.sh

---
+++ test/4.1.1.3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# 4.1.1.3 Ensure audit logs are not automatically deleted (Scored)
+
+cut -d\# -f2 /etc/audit/auditd.conf | grep 'max_log_file_action' | cut -d= -f2 | tr -d '[[:space:]]' | grep -q 'keep_logs' || exit 1
