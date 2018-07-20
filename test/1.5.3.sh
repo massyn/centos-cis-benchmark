225b2b9d5eb04d034aa21617d966094d49550208
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 13:45:06 +0900

    Update 1.5.3.sh

--- test/1.5.3.sh
+++ test/1.5.3.sh
@@ -4,4 +4,8 @@
 
 sysctl kernel.randomize_va_space | grep -E "kernel.randomize_va_space = 2" && exit $1
 
-grep "kernel\.randomize_va_space" /etc/sysctl.conf /etc/sysctl.d/* | grep -E "kernel.randomize_va_space = 2" || exit $1
+grep "kernel\.randomize_va_space" /etc/sysctl.conf | grep -E "kernel.randomize_va_space = 2" || exit $1
+
+if [[ -f /etc/sysctl.d/* ]]; then
+        grep "kernel\.randomize_va_space" /etc/sysctl.d/* | grep -E "kernel.randomize_va_space = 2" || exit $1
+fi
