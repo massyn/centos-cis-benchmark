f6211eaf18daa5234c9b0d5d6e3dd54c419143a7
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 09:16:10 +0900

    Create 5.4.1.1.sh

---
+++ test/5.4.1.1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# 5.4.1.1 Ensure password expiration is 365 days or less (Scored)
+
+PMD=$(grep -E "^PASS_MAX_DAYS" /etc/login.defs | awk {'print $2'})
+
+if [[ $PMD -eq '' || $PMD -gt 365 ]]; then
+        echo PASS_MAX_DAYS = $PMD
+        exit 1
+fi
+
+for i in $(egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1 ); do
+        UPA=$(chage --list $i | grep "Maximum number of days between password change" | cut -d: -f2)
+        if [[ $UPA -gt 365 ]]; then
+                echo $i maximum days between password change = $UPA
+                exit 1
+        fi
+done
