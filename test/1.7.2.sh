4eb98c85b4ee09cd04fff93c6aa156a49827212f
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 12:27:52 +0900

    Create 1.7.2.sh

---
+++ test/1.7.2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# 1.7.2 Ensure GDM login banner is configured (Scored)
+
+rpm -q gdm | grep -Eq "package gdm is not installed"
+if [[ $? -ne 0 ]]; then
+
+        if [[ -f /etc/dconf/profile/gdm ]]; then
+                cat /etc/dconf/profile/gdm | grep -E "user-db:user" || exit $?
+                cat /etc/dconf/profile/gdm | grep -E "system-db:gdm" || exit $?
+                cat /etc/dconf/profile/gdm | grep -E "file-db:/usr/share/gdm/greeter-dconf-defaults" || exit $?
+
+                if [[ -d /etc/dconf/db/gdm.d/ ]]; then
+                        grep "banner-message-enable=true" /etc/dconf/db/gdm.d/* || exit $?
+                else
+                        exit 1
+                fi
+        else
+                exit 1
+        fi
+fi
