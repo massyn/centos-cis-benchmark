53cd63f153d227b71b230b3c70760d5065998119
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 17:16:19 +0900

    Update 4.1.2.sh

--- test/4.1.2.sh
+++ test/4.1.2.sh
@@ -2,4 +2,4 @@
 
 # 4.1.2 Ensure auditd service is enabled (Scored)
 
-chkconfig --list auditd | grep -E "2:ons*3:ons*4:ons*5:on" || exit $?
+chkconfig --list auditd | grep -E "2:on\s*3:on\s*4:on\s*5:on" || exit $?
