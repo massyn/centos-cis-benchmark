a4c9fee40937141733e41af92fa8cf4a8d10d6b9
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 16:47:50 +0900

    Create 4.1.1.2.sh

---
+++ test/4.1.1.2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 4.1.1.2	Ensure system is disabled when audit logs are full (Scored)
+
+cut -d\# -f2 /etc/audit/auditd.conf | grep 'space_left_action' | cut -d= -f2 | tr -d '[[:space:]]' | grep -q 'email' || exit 1
+cut -d\# -f2 /etc/audit/auditd.conf | grep 'action_mail_acct' | cut -d= -f2 | tr -d '[[:space:]]' | grep -q 'root' || exit 1
+cut -d\# -f2 /etc/audit/auditd.conf | grep 'admin_space_left_action' | cut -d= -f2 | tr -d '[[:space:]]' | grep -q 'halt' || exit 1
