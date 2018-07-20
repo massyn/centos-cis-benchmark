7afed98d56322dc9249899f0d6f3ce3d3c915fd7
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 09:12:01 +0900

    Create 5.3.4.sh

---
+++ 5.3.4.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 5.3.4 Ensure password hashing algorithm is SHA-512 (Scored)
+
+egrep '^password\s+sufficient\s+pam_unix.so' /etc/pam.d/password-auth | grep sha512 || exit $?
+
+egrep '^password\s+sufficient\s+pam_unix.so' /etc/pam.d/system-auth | grep sha512 || exit $?
