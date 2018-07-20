c5a35beb1a9a60576e18c2d03b060351b9b4f023
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 16:46:14 +0900

    Create 3.7.sh

---
+++ test/3.7.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# 3.7 Ensure wireless interfaces are disabled (Not Scored)
+
+for i in $(iwconfig 2>&1 | egrep -v "no[[:space:]]*wireless" | cut -d' ' -f1); do
+	ip link show up | grep "${i}:"
+	if [[ "$?" -eq 0 ]]; then
+		return 1
+	fi
+done
