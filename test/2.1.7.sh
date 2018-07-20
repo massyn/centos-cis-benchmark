a6342c607a1bb53610876bc82d2a63c756e62858
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 12:42:49 +0900

    Create 2.1.7.sh

---
+++ test/2.1.7.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 2.1.7 Ensure talk server is not enabled (Scored)
+
+out=$(chkconfig --list | grep -E "talk")
+[[ -z "${out}" ]] || exit 1
+
+out=$(chkconfig --list | grep -E "talk" |grep ":on")
+[[ -z "${out}" ]] || exit 1
