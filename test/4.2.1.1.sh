cbe42daaa460973269f785e76e80f1c8f37693a8
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 17:15:45 +0900

    Update 4.2.1.1.sh

--- test/4.2.1.1.sh
+++ test/4.2.1.1.sh
@@ -2,4 +2,4 @@
 
 # 4.2.1.1 Ensure rsyslog Service is enabled (Scored)
 
-chkconfig --list rsyslog | grep -E "2:ons*3:ons*4:ons*5:on" || exit $?
+chkconfig --list rsyslog | grep -E "2:on\s*3:on\s*4:on\s*5:on" || exit $?
