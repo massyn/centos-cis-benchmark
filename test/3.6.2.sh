7775a5556a4d893936cd18400b94d531d4634517
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 16:26:11 +0900

    Create 3.6.2.sh

---
+++ test/3.6.2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 3.6.2 Ensure default deny firewall policy (Scored)
+
+iptables -L | egrep -q "Chain[[:space:]]+INPUT[[:space:]]+" | egrep -q "policy[[:space:]]+DROP" || exit 1
+iptables -L | egrep -q "Chain[[:space:]]+FORWARD[[:space:]]+" | egrep -q "policy[[:space:]]+DROP" || exit 1
+iptables -L | egrep -q "Chain[[:space:]]+OUTPUT[[:space:]]+" | egrep -q "policy[[:space:]]+DROP" || exit 1
