daf494208f5f29191f70a121a12f038c6d190316
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 11:44:26 +0900

    Create 1.6.1.6.sh

---
+++ test/1.6.1.6.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# 1.6.1.6 Ensure no unconfined daemons exist (Scored)
+
+out=$(ps -eZ | egrep "initrc" | egrep -vw "tr|ps|egrep|bash|awk" | tr ':' ' ' | awk '{ print $NF }')
+[[ -z "${out}" ]] || exit 1
