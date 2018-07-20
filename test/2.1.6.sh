c20b48582ca2f118efa4a4b3c2b5ae78b91d31ef
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 12:41:37 +0900

    Create 2.1.6.sh

---
+++ test/2.1.6.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 2.1.6 Ensure rsh server is not enabled (Scored)
+
+out=$(chkconfig --list | grep -E "rsh|rlogin|rexec")
+[[ -z "${out}" ]] || exit 1
+
+out=$(chkconfig --list | grep -E "rsh|rlogin|rexec" |grep ":on")
+[[ -z "${out}" ]] || exit 1
