7e2d2af4f7d15807ff237b0b2c0c3b6f95530b22
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 09:05:50 +0900

    Create 5.3.2.sh

---
+++ test/5.3.2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# 5.3.2 Ensure lockout for failed password attempts is configured (Scored)
+
+cat /etc/pam.d/password-auth | grep -E "auth\s*required\s*pam_faillock.so" || exit $?
+cat /etc/pam.d/password-auth | grep -E "auth\s*\[success=1\s*default=bad\]\s*pam_unix.so" || exit $?
+cat /etc/pam.d/password-auth | grep -E "auth\s*\[default=die\]\s*pam_faillock.so" ||exit $?
+cat /etc/pam.d/password-auth | grep -E "auth\s*sufficient\s*pam_faillock.so" || exit $?
+
+cat /etc/pam.d/system-auth | grep -E "auth\s*required\s*pam_faillock.so" || exit $?
+cat /etc/pam.d/system-auth | grep -E "auth\s*\[success=1\s*default=bad\]\s*pam_unix.so" || exit $?
+cat /etc/pam.d/system-auth | grep -E "auth\s*\[default=die\]\s*pam_faillock.so" ||exit $?
+cat /etc/pam.d/system-auth | grep -E "auth\s*sufficient\s*pam_faillock.so" || exit $?
