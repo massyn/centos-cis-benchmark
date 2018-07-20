e8951517657c15848732fcadc0c5c86d940ed543
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 17:15:13 +0900

    Update 4.2.2.1.sh

--- test/4.2.2.1.sh
+++ test/4.2.2.1.sh
@@ -2,4 +2,4 @@
 
 # 4.2.2.1 Ensure syslog-ng service is enabled (Scored)
 
-chkconfig --list syslog-ng | grep -E "2:ons*3:ons*4:ons*5:on" || exit $?
+chkconfig --list syslog-ng | grep -E "2:on\s*3:on\s*4:on\s*5:on" || exit $?
