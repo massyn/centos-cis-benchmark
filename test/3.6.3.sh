3db9678da83c4797162adab9e4ace3dca8bf85ba
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 16:27:30 +0900

    Create 3.6.3.sh

---
+++ test/3.6.3.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 3.6.3 Ensure loopback traffic is configured (Scored)
+
+accept="ACCEPT[[:space:]]+all[[:space:]]+--[[:space:]]+lo[[:space:]]+\*[[:space:]]+0\.0\.0\.0\/0[[:space:]]+0\.0\.0\.0\/0"
+drop="DROP[[:space:]]+all[[:space:]]+--[[:space:]]+\*[[:space:]]+\*[[:space:]]+127\.0\.0\.0\/8[[:space:]]+0\.0\.0\.0\/0"
+iptables -L INPUT -v -n | egrep -q ${accept} || exit 1
+iptables -L INPUT -v -n | egrep -q ${drop} || exit 1
+iptables -L OUTPUT -v -n | egrep -q ${accept} || exit 1
