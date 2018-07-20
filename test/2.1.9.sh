a473faf0e82e62c8ad01efc438380e42e5a1c7e5
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 12:44:46 +0900

    Create 2.1.9.sh

---
+++ test/2.1.9.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 2.1.9 Ensure tftp server is not enabled (Scored)
+
+out=$(chkconfig --list | grep -E "tftp")
+[[ -z "${out}" ]] || exit 1
+
+out=$(chkconfig --list | grep -E "tftp" |grep ":on")
+[[ -z "${out}" ]] || exit 1
