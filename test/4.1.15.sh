608a82cd953a8f62c40b86b19728aaf1ca023f95
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 17:02:47 +0900

    Create 4.1.15.sh

---
+++ test/4.1.15.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# 4.1.15 Ensure changes to system administration scope (sudoers) is collected (Scored)
+
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+scope" | egrep "\-p[[:space:]]+wa" \
+  | egrep -q "\-w[[:space:]]+\/etc\/sudoers" || exit 1
