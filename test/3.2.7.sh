bc4f5d81abafd73cb8ee81a9b6b9e434c06ee4b3
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 14:50:59 +0900

    Create 3.2.7.sh

---
+++ test/3.2.7.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# 3.2.7 Ensure Reverse Path Filtering is enabled (Scored)
+
+sysctl net.ipv4.conf.all.rp_filter | grep -E "net.ipv4.conf.all.rp_filter = 1" || exit $?
+
+grep "net.ipv4.conf.all.rp_filter" /etc/sysctl.conf | grep -E "net.ipv4.conf.all.rp_filter = 1" || exit $?
+
+if [[ -f /etc/sysctl.d/* ]]; then
+        grep "net.ipv4.conf.all.rp_filter" /etc/sysctl.d/* | grep -E "net.ipv4.conf.all.rp_filter = 1" || exit $?
+fi
+
+sysctl net.ipv4.conf.default.rp_filter | grep -E "net.ipv4.conf.default.rp_filter = 1" || exit $?
+
+grep "net.ipv4.conf.default.rp_filter" /etc/sysctl.conf | grep -E "net.ipv4.conf.default.rp_filter = 1" || exit $?
+
+if [[ -f /etc/sysctl.d/* ]]; then
+        grep "net.ipv4.conf.default.rp_filter" /etc/sysctl.d/* | grep -E "net.ipv4.conf.default.rp_filter = 1" || exit $?
+fi
