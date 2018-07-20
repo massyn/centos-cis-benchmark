18b5fd980df8918e0507a2be2b747f81860634f4
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 14:49:14 +0900

    Create 3.4.1.sh

---
+++ test/3.4.1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# 3.4.1 Ensure TCP Wrappers is installed (Scored)
+
+rpm -q tcp_wrappers |grep -E "tcp_wrappers-" || exit $?
+rpm -q tcp_wrappers-libs |grep -E "tcp_wrappers-libs-" || exit $?
