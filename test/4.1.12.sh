05de1b246d4bbc885572db55f78bcc8bd211f263
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 17:01:33 +0900

    Create 4.1.12.sh

---
+++ test/4.1.12.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# 4.1.12 Ensure use of privileged commands is collected (Scored)
+
+priv_cmds="$(find / -xdev \( -perm -4000 -o -perm -2000 \) -type f)"
+for cmd in ${priv_cmds} ; do
+	cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+privileged" | egrep "\-F[[:space:]]+path=${cmd}" \
+	| egrep "\-F[[:space:]]+perm=x" | egrep "\-F[[:space:]]+auid>=1000" | egrep "\-F[[:space:]]+auid\!=4294967295" \
+	| egrep -q "\-a[[:space:]]+always,exit|\-a[[:space:]]+exit,always" || exit 1
+done
