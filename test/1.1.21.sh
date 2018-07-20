70e939514ff4b97c700e8bb18f8a0739794ffba6
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 10:45:58 +0900

    Create 1.1.21.sh

---
+++ test/1.1.21.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# 1.1.21 Ensure sticky bit is set on all world-writable directories (Scored)
+
+dirs="$(df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \))"
+[[ -z "${dirs}" ]] || exit 1
