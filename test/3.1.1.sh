050dfa6268e3e430bc1c527ceaf71c0537370955
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 14:48:40 +0900

    Update 3.1.1.sh

--- test/3.1.1.sh
+++ test/3.1.1.sh
@@ -4,8 +4,8 @@
 
 sysctl net.ipv4.ip_forward | grep -E "net.ipv4.ip_forward = 0" || exit $?
 
-grep "net\.ipv4\.ip_forward" /etc/sysctl.conf | grep -E "net.ipv4.ip_forward = 0" || exit $?
+grep "net.ipv4.ip_forward" /etc/sysctl.conf | grep -E "net.ipv4.ip_forward = 0" || exit $?
 
 if [[ -f /etc/sysctl.d/* ]]; then
-        grep "net\.ipv4\.ip_forward" /etc/sysctl.d/* | grep -E "net.ipv4.ip_forward = 0" || exit $?
+        grep "net.ipv4.ip_forward" /etc/sysctl.d/* | grep -E "net.ipv4.ip_forward = 0" || exit $?
 fi
