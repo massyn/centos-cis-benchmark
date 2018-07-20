ad7a5402d15b4c1045d0215ceb384854dca40b4f
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 17:04:59 +0900

    Create 4.2.2.4.sh

---
+++ test/4.2.2.4.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# 4.2.2.4 Ensure syslog-ng is configured to send logs to a remote log host (Not Scored)
+
+egrep -q "destination[[:space:]]+logserver[[:space:]]+\{[[:space:]]*tcp\(\".+\"[[:space:]]+port\([[:digit:]]+\)\)\;[[:space:]]*\}\;" /etc/syslog-ng/syslog-ng.conf 2>/dev/null || exit 1
+egrep -q "log[[:space:]]+\{[[:space:]]*source\(src\)\;[[:space:]]*destination\(logserver\)\;[[:space:]]*\}\;" /etc/syslog-ng/syslog-ng.conf 2>/dev/null || exit 1
