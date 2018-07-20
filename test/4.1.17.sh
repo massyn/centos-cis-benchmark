310f909d2ca933935700c972bc320a8318222665
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 17:03:30 +0900

    Create 4.1.17.sh

---
+++ test/4.1.17.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# 4.1.17 Ensure kernel module loading and unloading is collected (Scored)
+
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+modules" | egrep "\-p[[:space:]]+x" \
+  | egrep -q "\-w[[:space:]]+\/sbin\/insmod" || exit 1
+
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+modules" | egrep "\-p[[:space:]]+x" \
+  | egrep -q "\-w[[:space:]]+\/sbin\/rmmod" || exit 1
+
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+modules" | egrep "\-p[[:space:]]+x" \
+  | egrep -q "\-w[[:space:]]+\/sbin\/modprobe" || exit 1
+
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+modules" | egrep "\-S[[:space:]]+delete_module" \
+  | egrep "\-F[[:space:]]+arch=b64" | egrep "\-S[[:space:]]+init_module" \
+  | egrep -q "\-a[[:space:]]+always,exit|\-a[[:space:]]+exit,always" || exit 1
