415273bae0f4ee73506c52fdc73d1cdf048be2cf
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 16:58:44 +0900

    Create 4.1.7.sh

---
+++ test/4.1.7.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# 4.1.7 Ensure events that modify the system's Mandatory Access Controls are collected (Scored)
+
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+MAC-policy" | egrep "\-p[[:space:]]+wa" \
+  | egrep -q "\-w[[:space:]]+\/etc\/selinux\/" || exit 1
