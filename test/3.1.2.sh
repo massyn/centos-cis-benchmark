ec4f6a72189ca2eaa534cf742bd40fa8358cbb11
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 14:48:03 +0900

    Update 3.1.2.sh

--- test/3.1.2.sh
+++ test/3.1.2.sh
@@ -4,24 +4,16 @@
 
 sysctl net.ipv4.conf.all.send_redirects | grep -E "net.ipv4.conf.all.send_redirects = 0" || exit $?
 
-sysctl net.ipv4.conf.default.send_redirects | grep -E "net.ipv4.conf.default.send_redirects = 0" || exit $?
-
-grep "net\.ipv4\.conf\.all\.send_redirects" /etc/sysctl.conf | grep -E "net.ipv4.conf.all.send_redirects = 0" || exit $?
-grep "net\.ipv4\.conf\.default\.send_redirects" /etc/sysctl.conf |grep -E "net.ipv4.conf.default.send_redirects = 0" || exit $?
+grep "net.ipv4.conf.all.send_redirects" /etc/sysctl.conf | grep -E "net.ipv4.conf.all.send_redirects = 0" || exit $?
 
 if [[ -f /etc/sysctl.d/* ]]; then
-        grep "net\.ipv4\.conf\.all\.send_redirects" /etc/sysctl.d/* | grep -E "net.ipv4.conf.all.send_redirects = 0" || exit $?
-    grep "net\.ipv4\.conf\.default\.send_redirects" /etc/sysctl.d/* |grep -E "net.ipv4.conf.default.send_redirects = 0" || exit $?
+        grep "net.ipv4.conf.all.send_redirects" /etc/sysctl.d/* | grep -E "net.ipv4.conf.all.send_redirects = 0" || exit $?
 fi
 
+sysctl net.ipv4.conf.default.send_redirects | grep -E "net.ipv4.conf.default.send_redirects = 0" || exit $?
 
-
-
-
-
-
-grep "net\.ipv4\.ip_forward" /etc/sysctl.conf | grep -E "net.ipv4.ip_forward = 0" || exit $?
+grep "net.ipv4.conf.default.send_redirects" /etc/sysctl.conf | grep -E "net.ipv4.conf.default.send_redirects = 0" || exit $?
 
 if [[ -f /etc/sysctl.d/* ]]; then
-        grep "net\.ipv4\.ip_forward" /etc/sysctl.d/* | grep -E "net.ipv4.ip_forward = 0" || exit $?
+        grep "net.ipv4.conf.default.send_redirects" /etc/sysctl.d/* | grep -E "net.ipv4.conf.default.send_redirects = 0" || exit $?
 fi
