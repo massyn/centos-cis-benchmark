289cec1493593066f8e44f2d42e4fc3d6194d5b4
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 17:03:05 +0900

    Create 4.1.16.sh

---
+++ test/4.1.16.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# 4.1.16 Ensure system administrator actions (sudolog) are collected (Scored)
+
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+actions" | egrep "\-p[[:space:]]+wa" \
+  | egrep -q "\-w[[:space:]]+\/var\/log\/sudo.log" || exit 1
