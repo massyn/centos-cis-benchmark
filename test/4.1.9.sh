856beac3c266fd73320f9c9e457248ddce0230c9
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 16:59:40 +0900

    Create 4.1.9.sh

---
+++ test/4.1.9.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# 4.1.9 Ensure session initiation information is collected (Scored)
+
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+session" | egrep "\-p[[:space:]]+wa" \
+  | egrep -q "\-w[[:space:]]+\/var\/run\/utmp" || exit 1
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+session" | egrep "\-p[[:space:]]+wa" \
+  | egrep -q "\-w[[:space:]]+\/var\/log\/wtmp" || exit 1
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+session" | egrep "\-p[[:space:]]+wa" \
+  | egrep -q "\-w[[:space:]]+\/var\/log\/btmp" || exit 1
