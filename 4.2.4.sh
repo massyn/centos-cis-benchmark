ea0a2e83cf92f8943bd1646be028693b540cac11
Phil Massyn <massyn@users.noreply.github.com>
2018-07-20 09:42:26 +0900

    Create 4.2.4.sh

---
+++ 4.2.4.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# 4.2.4 Ensure permissions on all logfiles are configured (Scored)
+
+[[ $(find /var/log -type f -ls | grep -v "\-r\-\-\-\-\-\-\-\-" | grep -v "\-rw\-\-\-\-\-\-\-" | grep -v "\-rw\-r\-\-\-\-\-" | wc -l) -eq 0 ]] || exit 1
