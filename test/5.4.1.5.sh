fea39367037c293772c11925cc4a071830733271
Phil Massyn <massyn@users.noreply.github.com>
2018-07-21 13:04:55 +0900

    Create 5.4.1.5.sh

---
+++ test/5.4.1.5.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 5.4.1.5 Ensure all users last password change date is in the past (Scored)
+
+NOW=$(date +%s)
+for i in $(egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1 ); do
+        UPA=$(chage --list $i | grep "Last password change" | cut -d: -f2)
+        EPOCH=$(date -d "$UPA" +%s)
+
+        if [[ $EPOCH -gt $NOW ]]; then
+                echo Password change in future - $i - $UPA
+                exit 1
+        fi
+done
