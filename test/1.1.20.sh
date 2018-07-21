3fcbf8be54f4d35361703905eb5713a6d26f39ba
Phil Massyn <massyn@users.noreply.github.com>
2018-07-21 13:51:19 +0900

    Create 1.1.20.sh

---
+++ test/1.1.20.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# 1.1.20 Ensure noexec option set on removable media partitions (Not Scored)
+
+MEDIA=$(mount -l -t vfat,iso9660,ext)
+
+if [[ -z $MEDIA ]]; then
+        exit 0
+else
+        echo $MEDIA | grep "noexec" || exit $?
+fi
