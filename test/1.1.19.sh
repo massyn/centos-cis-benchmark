cb738f6727eb56179658874dad6ce4d8680559af
Phil Massyn <massyn@users.noreply.github.com>
2018-07-21 13:50:15 +0900

    Create 1.1.19.sh

---
+++ test/1.1.19.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# 1.1.19 Ensure nosuid option set on removable media partitions (Not Scored)
+
+MEDIA=$(mount -l -t vfat,iso9660,ext)
+
+if [[ -z $MEDIA ]]; then
+        exit 0
+else
+        echo $MEDIA | grep "nosuid" || exit $?
+fi
