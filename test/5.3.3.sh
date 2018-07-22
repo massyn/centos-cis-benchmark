da01deec9870da85d4ee89d980f227503962277a
Phil Massyn <massyn@users.noreply.github.com>
2018-07-22 21:14:57 +0900

    Create 5.3.3.sh

---
+++ test/5.3.3.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# 5.3.3 Ensure password reuse is limited (Scored)
+
+R=$(egrep '^password\s+sufficient\s+pam_unix.so' /etc/pam.d/password-auth | grep "remember" | sed 's/.*remember=\([0-9]*\).*/\1/g')
+if [[ $R -lt 5 ]] ; then
+        exit 1
+fi
+
+R=$(egrep '^password\s+sufficient\s+pam_unix.so' /etc/pam.d/system-auth |grep "remember" | sed 's/.*remember=\([0-9]*\).*/\1/g')
+if [[ $R -lt 5 ]] ; then
+        exit 1
+fi
