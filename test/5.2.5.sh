29a2deaedb9060e8f636ddb3d1b8cb00199f5650
Phil Massyn <massyn@gmail.com>
2018-07-23 21:56:21 +0900

    bug

--- test/5.2.5.sh
+++ test/5.2.5.sh
@@ -3,7 +3,7 @@
 
 # 5.2.5 - Ensure SSH MaxAuthTries is set to 4 or less (Scored)
 
-MAT=$(grep "^MaxAuthTries" /etc/ssh/sshd_config |awk {'print $1'})
+MAT=$(grep "^MaxAuthTries" /etc/ssh/sshd_config |awk {'print $2'})
 
 if [[ $MAT -eq '' || $MAT -gt 4 ]]; then
         echo MaxAuthTries = $MAT
