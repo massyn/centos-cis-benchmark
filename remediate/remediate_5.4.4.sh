5d34871576590950007a1a7bae1c1bf13f8483f9
Phil Massyn <massyn@gmail.com>
2018-07-23 22:03:17 +0900

    bug fixes

--- remediate/remediate_5.4.4.sh
+++ remediate/remediate_5.4.4.sh
@@ -3,4 +3,5 @@
 
 # 5.4.4 - Ensure default user umask is 027 or more restrictive (Scored)
 
-[[ "$(umask)" =~ [0-7][2-7]7$ ]] || exit;
+echo umask 027 >> /etc/bashrc
+echo umask 027 >> /etc/profile
--- remediate/remediate_5.4.5.sh
+++ remediate/remediate_5.4.5.sh
@@ -3,6 +3,5 @@
 
 # 5.4.5 - Ensure default user shell timeout is 900 seconds or less (Scored)
 
-if [[ $TMOUT -eq "" || $TMOUT -gt 900 ]]; then
-        exit 1
-fi
+echo TMOUT=600>>/etc/bashrc
+echo TMOUT=600>>/etc/profile
