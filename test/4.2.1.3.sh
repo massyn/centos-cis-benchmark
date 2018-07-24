c02b11888d034b8d8251c7c0cf2ef5d10e0c48c7
Phil Massyn <massyn@users.noreply.github.com>
2018-07-24 09:20:48 +0900

    Create 4.2.1.3.sh

---
+++ test/4.2.1.3.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# 4.2.1.3 Ensure rsyslog default file permissions configured (Scored)
+
+# remediate
+#echo "\$FileCreateMode 0640">>/etc/rsyslog.conf
+
+EC=0
+[[ "$(grep ^\$FileCreateMode /etc/rsyslog.conf)" =~ 0[0-6][0-4]0$ ]] || EC=1
+if [[ $EC == 0 ]]; then
+        exit 0
+fi
+
+if [[ $(ls -A /etc/rsyslog.d/) ]] ; then
+        [[ "$(grep ^\$FileCreateMode /etc/rsyslog.d/*.conf)" =~ 0[0-6][0-4]0$ ]] || EC=1
+        if [[ $EC == 0 ]]; then
+                exit 0
+        fi
+fi
