32c78a8296c809101e18c2e13d13fa57172a9149
Phil Massyn <massyn@users.noreply.github.com>
2018-07-24 15:23:34 +0900

    Create 4.3.sh

---
+++ test/4.3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# 4.3 Ensure logrotate is configured (Not Scored)
+
+cat /etc/logrotate.conf | tr '\n' '\r' | sed -e 's/{.*}//g' | tr '\r' '\n' | grep -v "^\s*#" | grep "maxage" || exit $?
