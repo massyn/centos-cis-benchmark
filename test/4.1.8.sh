1620050d86a740db024428d442a1dcdd358de987
pauluss75 <62563443+pauluss75@users.noreply.github.com>
2020-03-23 18:28:14 +0100

    [FIX][TYPO]
    Searching for /var/run/faillock as in CIS benchmark instead of /var/run/faillog

--- test/4.1.8.sh
+++ test/4.1.8.sh
@@ -3,5 +3,5 @@
 
 # 4.1.8 - Ensure login and logout events are collected (Scored)
 
-cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+logins" | egrep "\-p[[:space:]]+wa"   | egrep -q "\-w[[:space:]]+\/var\/run\/faillog" || exit 1
+cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+logins" | egrep "\-p[[:space:]]+wa"   | egrep -q "\-w[[:space:]]+\/var\/run\/faillock" || exit 1
 cut -d\# -f1 /etc/audit/audit.rules | egrep "\-k[[:space:]]+logins" | egrep "\-p[[:space:]]+wa" | egrep -q "\-w[[:space:]]+\/var\/log\/lastlog" || exit 1
